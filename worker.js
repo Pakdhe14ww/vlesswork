b60ff6cb5ab8c35b66ab5daed3df1b965caf2f6a
Pakdhe14ww <148045184+Pakdhe14ww@users.noreply.github.com>
2023-11-29 23:15:08 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -198,7 +198,7 @@ var cf_worker_vless_default = {
     async fetch(request, env, ctx) {
         let address = "";
         let portWithRandomLog = "";
-        const userID = env.UUID || "00fdxxxd-81xx-xx54-bxx2-b6xxx2aXXXXX"; // ENTER YOUR OWN UUID HERE
+        const userID = env.UUID || "f2298581-16d6-4c42-907d-3db31b64c38a"; // ENTER YOUR OWN UUID HERE
         const isVaildUUID = validate_default(userID);
         const log = (info, event) => {
             console.log(`[${address}:${portWithRandomLog}] ${info}`, event || "");
